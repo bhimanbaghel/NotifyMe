e4bec637dde7d6df27642aba6cfb62d3d1be88d7
Bhiman Kumar Baghel <bkb45@Bhimans-MacBook-Pro.local>
2025-06-05 14:31:04 -0400

    removed basename NotifyMe

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/NotifyMe">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>

1c889c1c7423625cd36b10c113e28634165abc07
Bhiman Kumar Baghel <bkb45@Bhimans-MacBook-Pro.local>
2025-06-05 15:58:25 -0400

    updated webpack to get correct env

--- webpack.config.js
+++ webpack.config.js
@@ -3,51 +3,53 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
 const webpack = require('webpack');
 
-// Check if we're in production mode
-const isProduction = process.env.NODE_ENV === 'production';
+module.exports = (env, argv) => {
+  // Check if we're in production mode from the webpack mode argument
+  const isProduction = argv.mode === 'production';
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-    // Use absolute path for GitHub Pages
-    publicPath: isProduction ? '/NotifyMe/' : '/'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader'
+  return {
+    entry: './src/index.js',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'bundle.js',
+      // Use absolute path for GitHub Pages
+      publicPath: isProduction ? '/NotifyMe/' : '/'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader'
+          }
+        },
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader']
         }
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      }
-    ]
-  },
-  resolve: {
-    extensions: ['.js', '.jsx']
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-      // Add base href for GitHub Pages
-      ...(isProduction ? {
-        base: '/NotifyMe/'
-      } : {})
-    }),
-    new Dotenv(),
-    new webpack.DefinePlugin({
-        'process.env.PUBLIC_URL': JSON.stringify(isProduction ? '/NotifyMe' : '')
-    })
-  ],
-  devServer: {
-    historyApiFallback: true,
-    port: 3000,
-    hot: true
-  }
+      ]
+    },
+    resolve: {
+      extensions: ['.js', '.jsx']
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+        // Add base href for GitHub Pages
+        ...(isProduction ? {
+          base: '/NotifyMe/'
+        } : {})
+      }),
+      new Dotenv(),
+      new webpack.DefinePlugin({
+          'process.env.PUBLIC_URL': JSON.stringify(isProduction ? '/NotifyMe' : '')
+      })
+    ],
+    devServer: {
+      historyApiFallback: true,
+      port: 3000,
+      hot: true
+    }
+  };
 }; 
